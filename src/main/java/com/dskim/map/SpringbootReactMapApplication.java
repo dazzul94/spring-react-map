1ef8a3a6516b4c0d0ef852d9c44e15d744f33f50
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2019-06-30 00:14:25 +0900

    springboot project 생성

---
+++ src/main/java/com/dskim/map/SpringbootReactMapApplication.java
@@ -0,0 +1,13 @@
+package com.dskim.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactMapApplication.class, args);
+	}
+
+}
