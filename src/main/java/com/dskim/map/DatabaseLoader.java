086f2808376e8d3f6f52ded96ca4d68305cd8877
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2019-07-01 01:54:52 +0900

    초기 데이터 적재

--- src/main/java/com/dskim/map/DatabaseLoader.java
+++ src/main/java/com/dskim/map/DatabaseLoader.java
@@ -27,38 +27,24 @@ public class DatabaseLoader implements CommandLineRunner {
 		/* 초기 데이터 적재 */
 		UserAccount admin = this.userAccounts.save(new UserAccount("admin", "1111",
 				"ADMIN"));
+		UserAccount admin2 = this.userAccounts.save(new UserAccount("admin2", "2222",
+				"ADMIN"));
+		
 		SecurityContextHolder.getContext().setAuthentication(
 				new UsernamePasswordAuthenticationToken("admin", "doesn't matter",
 					AuthorityUtils.createAuthorityList("ADMIN")));
+		SecurityContextHolder.getContext().setAuthentication(
+				new UsernamePasswordAuthenticationToken("admin2", "doesn't matter",
+					AuthorityUtils.createAuthorityList("ADMIN")));
 		
-		// this.searchHistories.save(new SearchHistory("디아뜨", admin));
-		// this.searchHistories.save(new SearchHistory("카카오", admin));
-		// this.searchHistories.save(new SearchHistory("할리스", admin));
+		this.searchHistories.save(new SearchHistory("카페베네", admin));
+		this.searchHistories.save(new SearchHistory("스타벅스", admin));
+		this.searchHistories.save(new SearchHistory("할리스", admin));
+		
+		this.searchHistories.save(new SearchHistory("롯데리아", admin2));
+		this.searchHistories.save(new SearchHistory("맥도날드", admin2));
+		this.searchHistories.save(new SearchHistory("버거킹", admin2));
 		
 		SecurityContextHolder.clearContext();
-		
-//		Manager greg = this.managers.save(new Manager("greg", "turnquist",
-//							"ROLE_MANAGER"));
-//		Manager oliver = this.managers.save(new Manager("oliver", "gierke",
-//							"ROLE_MANAGER"));
-//
-//		SecurityContextHolder.getContext().setAuthentication(
-//			new UsernamePasswordAuthenticationToken("greg", "doesn't matter",
-//				AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
-//
-//		this.employees.save(new Employee("Frodo", "Baggins", "ring bearer", greg));
-//		this.employees.save(new Employee("Bilbo", "Baggins", "burglar", greg));
-//		this.employees.save(new Employee("Gandalf", "the Grey", "wizard", greg));
-//
-//		SecurityContextHolder.getContext().setAuthentication(
-//			new UsernamePasswordAuthenticationToken("oliver", "doesn't matter",
-//				AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
-//
-//		this.employees.save(new Employee("Samwise", "Gamgee", "gardener", oliver));
-//		this.employees.save(new Employee("Merry", "Brandybuck", "pony rider", oliver));
-//		this.employees.save(new Employee("Peregrin", "Took", "pipe smoker", oliver));
-//
-//		SecurityContextHolder.clearContext();
 	}
 }
-// end::code[]
