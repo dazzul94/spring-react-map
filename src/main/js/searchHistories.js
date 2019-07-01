56eaf8a983457011c0030e9f694b81fc50a8c24d
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2019-07-01 15:45:45 +0900

    Update searchHistories.js

--- src/main/js/searchHistories.js
+++ src/main/js/searchHistories.js
@@ -9,27 +9,28 @@ class SearchHistories extends React.Component {
 
     render() {
         const { searchHistories, loggedInUser } = this.props;
+        const filteredSearchHistories = (searchHistories && searchHistories.length > 0)?
+            searchHistories.filter( searchHistory => {
+                return (
+                    searchHistory.entity.userAccount.name === loggedInUser
+                )
+            }).
+            sort( (a,b) => {
+                return new Date(b.entity.createdDtm) - new Date(a.entity.createdDtm);
+            }):[];
         return (
             <div id="recommend" className="invisible">
-                {(searchHistories && searchHistories.length > 0)?
-						searchHistories.filter( searchHistory => {
-							return (
-                                searchHistory.entity.userAccount.name === loggedInUser
-							)
-                        }).
-                        sort( (a,b) => {
-                            return new Date(b.entity.createdDtm) - new Date(a.entity.createdDtm);
-                        })
-                        .map( (searchHistory, idx) =>{
-                            return (
-                                <div key={idx} className="item">
-                                    {searchHistory.entity.keyWord}({searchHistory.entity.createdDtm}) <span className="text"></span>
-                                </div>
-                            )
+                {filteredSearchHistories && filteredSearchHistories.length > 0?
+                    filteredSearchHistories.map( (searchHistory, idx) =>{
+                        return (
+                            <div key={idx} className="item">
+                                {searchHistory.entity.keyWord}({searchHistory.entity.createdDtm}) <span className="text"></span>
+                            </div>
+                        )
 
-                        })
-						:<div className="item"><span className="text">최근 검색 이력이 없습니다.</span></div>
-					}
+                    })
+                    :<div className="item"><span className="text">최근 검색 이력이 없습니다.</span></div>
+                }
             </div>
         )
 	}
